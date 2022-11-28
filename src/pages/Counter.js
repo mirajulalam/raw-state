10e3f5bf954abb7ec8b4f7c0aecb732aeb4911dc
mirajulalam <alifmirajulalam@gmail.com>
2022-11-28 11:53:48 -0800

    counter components action payload added

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -6,10 +6,10 @@ const Counter = () => {
 
     const reducer =(state,action)=>{
         if(action.type === "INCREMENT"){
-            return state +1;
+            return state + action.payload;
         }
         else if(action.type === "DECREMENT"){
-            return state -1;
+            return state - action.payload;
         }
     }
 
@@ -26,13 +26,13 @@ const Counter = () => {
                     <div class="flex space-x-3">
                         <button
                             class="bg-red-400 text-white px-3 py-2 rounded shadow"
-                            id="decrement" onClick={()=> dispatch({type:"DECREMENT"})}
+                            id="decrement" onClick={()=> dispatch({type:"DECREMENT", payload: 5})}
                         >
                             Decrement
                         </button>
                         <button
                             class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                            id="increment" onClick={()=> dispatch({type:"INCREMENT"})}
+                            id="increment" onClick={()=> dispatch({type:"INCREMENT", payload: 5})}
                         >
                             Increment
                         </button>
